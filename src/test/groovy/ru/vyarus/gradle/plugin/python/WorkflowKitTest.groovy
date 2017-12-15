34fd7bf283521c24034f8e59b3c2095a4cd469be
Vyacheslav Rusakov <vyarus@gmail.com>
2017-12-15 19:55:11 +0700

    workflow tests

--- src/test/groovy/ru/vyarus/gradle/plugin/python/WorkflowKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/python/WorkflowKitTest.groovy
@@ -10,22 +10,193 @@ import ru.vyarus.gradle.plugin.python.cmd.Virtualenv
  */
 class WorkflowKitTest extends AbstractKitTest {
 
-    def "No pip modules - no env used"() {
+    def "Check no pip modules - no env created"() {
 
         setup:
+        Virtualenv env = env()
         build """
             plugins {
                 id 'ru.vyarus.use-python'
             }
         """
-        Virtualenv env = env()
 
         when: "run task"
         BuildResult result = run('checkPython')
 
-        then: "no env created"
+        then: "no env created (global binary used)"
         result.task(':checkPython').outcome == TaskOutcome.SUCCESS
-        !result.output.contains('python -m virtualenv .gradle/python')
+        result.output =~ / \(python(3)?\)/
         !env.exists()
+    }
+
+    def "Check no pip modules - existing env used"() {
+
+        setup: "create env with default path so it could be detected"
+        Virtualenv env = env()
+        env.createPythonOnly()
+        build """
+            plugins {
+                id 'ru.vyarus.use-python'
+            }
+        """
+
+        when: "run task"
+        BuildResult result = run('checkPython')
+
+        then: "existing env used"
+        result.task(':checkPython').outcome == TaskOutcome.SUCCESS
+        result.output.contains(env.pythonPath.replace('/', '\\'))
+    }
+
+    def "Check restricted user scope - existing env not used"() {
+
+        setup: "create recognizable env but force user scope"
+        Virtualenv env = env()
+        env.createPythonOnly()
+        build """
+            plugins {
+                id 'ru.vyarus.use-python'
+            }
+            
+            python.scope = USER
+        """
+
+        when: "run task"
+        BuildResult result = run('checkPython')
+
+        then: "existing env not used"
+        result.task(':checkPython').outcome == TaskOutcome.SUCCESS
+        result.output =~ / \(python(3)?\)/
+    }
+
+    def "Check use existing env"() {
+
+        setup: "create env inti not default pat and reconfigure plugin to recognize it"
+        Virtualenv env = env('env')
+        env.createPythonOnly()
+        build """
+            plugins {
+                id 'ru.vyarus.use-python'
+            }
+            
+            python.envPath = 'env'
+        """
+
+        when: "run task"
+        BuildResult result = run('checkPython')
+
+        then: "existing env used"
+        result.task(':checkPython').outcome == TaskOutcome.SUCCESS
+        result.output.contains(env.pythonPath.replace('/', '\\'))
+    }
+
+    def "Check prevent virtualenv installation"() {
+
+        setup: "create undetectable virtualenv and use it as pure python"
+        Virtualenv env = env('env')
+        env.createPythonOnly()
+        build """
+            plugins {
+                id 'ru.vyarus.use-python'
+            }
+                        
+            python.pythonPath = '$env.pythonPath'
+            python.installVirtualenv = false
+        """
+
+        when: "run task"
+        BuildResult result = run('checkPython')
+
+        then: "virtualenv not installed"
+        result.task(':checkPython').outcome == TaskOutcome.SUCCESS
+        !result.output.contains('-m pip install virtualenv')
+    }
+
+    def "Check copy env"() {
+
+        setup:
+        Virtualenv env = env()
+        build """
+            plugins {
+                id 'ru.vyarus.use-python'
+            }
+                     
+            python.envCopy = true
+            python.pip 'click:6.7'
+        """
+
+        when: "run task"
+        BuildResult result = run('checkPython')
+
+        then: "env created, but not symlinked"
+        result.task(':checkPython').outcome == TaskOutcome.SUCCESS
+        result.output.contains('--always-copy')
+        env.exists()
+    }
+
+    def "Check pip not installed"() {
+
+        setup: "use custom env without python as pure python"
+        Virtualenv env = env('env')
+        env.createPythonOnly()
+        build """
+            plugins {
+                id 'ru.vyarus.use-python'
+            }
+                        
+            python.pythonPath = '$env.pythonPath'            
+            python.pip 'click:6.7'
+        """
+
+        when: "run task"
+        BuildResult result = runFailed('checkPython')
+
+        then: "error - pip not installed"
+        result.output.contains('Pip is not installed')
+    }
+
+    def "Check environment without pip"() {
+
+        setup: "detectable environment did not contain pip"
+        Virtualenv env = env()
+        env.createPythonOnly()
+        build """
+            plugins {
+                id 'ru.vyarus.use-python'
+            }
+                        
+            python.pip 'click:6.7'
+        """
+
+        when: "run task"
+        BuildResult result = runFailed('checkPython')
+
+        then: "error - pip not installed"
+        result.output.contains('Pip is not installed')
+    }
+
+    def "Check virtualenv required"() {
+
+        setup: "virtualenv used as usual python (path not match configured envPath)"
+        Virtualenv env = env('env')
+        env.create()
+        build """
+            plugins {
+                id 'ru.vyarus.use-python'
+            }
+                        
+            python {
+                pythonPath = '$env.pythonPath'
+                installVirtualenv = false
+                scope = VIRTUALENV             
+                pip 'click:6.7'
+            }
+        """
+
+        when: "run task"
+        BuildResult result = runFailed('checkPython')
+
+        then: "virtualenv not found"
+        result.output.contains('Virtualenv is not installed')
     }
 }

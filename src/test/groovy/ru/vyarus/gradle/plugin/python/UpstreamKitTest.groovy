65b18dd337d4386803e77bbc64ae4f26d75dd243
Vyacheslav Rusakov <vyarus@gmail.com>
2021-07-01 02:03:56 +0700

    update upstream test to gradle 7.1

--- src/test/groovy/ru/vyarus/gradle/plugin/python/UpstreamKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/python/UpstreamKitTest.groovy
@@ -9,7 +9,7 @@ import org.gradle.testkit.runner.TaskOutcome
  */
 class UpstreamKitTest extends AbstractKitTest {
 
-    String GRADLE_VERSION = '6.8.2'
+    String GRADLE_VERSION = '7.1'
 
     def "Check simple plugin execution"() {
         setup:
